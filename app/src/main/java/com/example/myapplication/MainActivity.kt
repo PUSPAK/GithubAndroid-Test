43f2437b3f1ed11bef2faa0a1a7622653656ae7c
Puspak <pudasgupta@paypal.com>
2022-02-20 16:27:56 +0530

    Testing Android Github
    squash! Testing Android Github

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,8 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         System.out.println("Hello World")
         System.out.println("Hello World2")
-        System.out.println("Hello World 3")
-        System.out.println("Hello World 4")
 
     }
 }
